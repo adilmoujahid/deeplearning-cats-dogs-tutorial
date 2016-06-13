22f30aecbc37407f76232e32a8c6ab41ef059269
Adil Moujahid <adil.mouja@gmail.com>
2016-06-14 00:01:02 +0900

    First commit

---
+++ code/create_lmdb.py
@@ -0,0 +1,107 @@
+import os
+import glob
+import random
+import numpy as np
+
+import cv2
+
+import caffe
+import lmdb
+
+from caffe.proto import caffe_pb2
+
+
+#Size of images
+IMAGE_WIDTH = 227
+IMAGE_HEIGHT = 227
+
+def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
+
+    #Histogram Equalization
+    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+
+    #Image Resizing
+    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+
+    return img
+
+
+def make_datum(img, label):
+    #image is numpy.ndarray format. BGR instead of RGB
+    return caffe_pb2.Datum(
+        channels=3,
+        width=IMAGE_WIDTH,
+        height=IMAGE_HEIGHT,
+        label=label,
+        data=np.rollaxis(img, 2).tostring())
+
+train_lmdb = '/home/ubuntu/cats-dogs-tutorial/input/train_lmdb'
+validation_lmdb = '/home/ubuntu/cats-dogs-tutorial/input/validation_lmdb'
+test_lmdb = '/home/ubuntu/cats-dogs-tutorial/input/test_lmdb'
+
+os.system('rm -rf  ' + train_lmdb)
+os.system('rm -rf  ' + validation_lmdb)
+os.system('rm -rf  ' + test_lmdb)
+
+
+train_data = [img for img in glob.glob("../input/train/*jpg")]
+test_data = [img for img in glob.glob("../input/test1/*jpg")]
+
+#Shuffle train_data
+random.shuffle(train_data)
+
+print 'Creating train_lmdb'
+
+in_db = lmdb.open(train_lmdb, map_size=int(1e12))
+with in_db.begin(write=True) as in_txn:
+    for in_idx, img_path in enumerate(train_data):
+        if in_idx %  6 == 0:
+            continue
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+        if 'cat' in img_path:
+            label = 0
+        else:
+            label = 1
+        datum = make_datum(img, label)
+        in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
+        print '{:0>5d}'.format(in_idx) + ':' + img_path
+in_db.close()
+
+
+print '\nCreating validation_lmdb'
+
+in_db = lmdb.open(validation_lmdb, map_size=int(1e12))
+with in_db.begin(write=True) as in_txn:
+    for in_idx, img_path in enumerate(train_data):
+        if in_idx % 6 != 0:
+            continue
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+        if 'cat' in img_path:
+            label = 0
+        else:
+            label = 1
+        datum = make_datum(img, label)
+        in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
+        print '{:0>5d}'.format(in_idx) + ':' + img_path
+in_db.close()
+
+
+print '\nCreating test_lmdb'
+
+in_db = lmdb.open(test_lmdb, map_size=int(1e12))
+with in_db.begin(write=True) as in_txn:
+    for in_idx, img_path in enumerate(test_data):
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+        label = 0
+        datum = make_datum(img, label)
+        in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
+        print '{:0>5d}'.format(in_idx) + ':' + img_path
+in_db.close()
+
+
+print '\nFinished processing all images'
